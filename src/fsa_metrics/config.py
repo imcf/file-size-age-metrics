ca774964f6f83f4619cfb3c59d98dcd58cbf87b0
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2025-01-31 18:36:17 +0100

    Cleanups

--- src/fsa_metrics/config.py
+++ src/fsa_metrics/config.py
@@ -9,11 +9,12 @@ def load_config_file(filename):
     if "port" not in config.keys():
         config.port = "16061"
     if "fsa_dir" not in config.keys():
-        config.fsa_dir = "/var/backups/"
+        config.fsa_dir = "/var/backups"
     if "pattern" not in config.keys():
         config.pattern = "**"
     if "interval" not in config.keys():
         config.interval = 60
     if "verbosity" not in config.keys():
         config.verbosity = 0
+
     return config
